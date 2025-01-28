275617389491cafbb37413162fd6ee8e619c7854
danis <ahrnadeev@gmail.com>
2025-01-28 23:41:54 +0300

    Minor improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,24 @@
 import Navbar from "../components/Navbar/Navbar.jsx";
 import styles from "../page-styles/Home.module.css";
 import {useEffect, useState} from "react";
-import DataTable from "../components/Table/DataTable.jsx";
 import Modal from "../components/Modal/Modal.jsx";
 import {crudCreate, crudRead, crudUpdate, crudDelete, crudReadMany, crudDeleteMany} from "../utils/crud.js";
 import DragonTable from "../components/Table/DragonTable.jsx";
 import CreateDragon from "../components/CreateDragon/CreateDragon.jsx";
-import Alert from "../components/Alert/Alert.jsx";
 import {useAuth} from "../components/utils/AuthProvider.jsx";
 
 function Home({ pageTitle }) {
+    const BASE_URL = "http://localhost:8080/backend-jakarta-ee-1.0-SNAPSHOT/api/user";
 
     const { logout } = useAuth();
 
-    const [modalActive, setModalActive] = useState(false);
     const [createDragonModalActive, setCreateDragonModalActive] = useState(false);
 
-    const [alertActive, setAlertActive] = useState(false);
-
     const [tableReload, setTableReload] = useState(false);
-
-    const showAlert = () => {
-        setAlertActive(true);
-    };
-
 
     useEffect(() => {
         document.title = pageTitle;
     })
-
-    const BASE_URL = "http://localhost:8080/backend-jakarta-ee-1.0-SNAPSHOT/api/user";
 
     const loadDataWrapper = async (func, args) => {
         try {
@@ -69,8 +58,6 @@ function Home({ pageTitle }) {
                 </h1>
 
                 <button onClick={() => setCreateDragonModalActive(true)}>CREATE POPUP</button>
-                <button onClick={() => setModalActive(true)}>Открыть модальное окно</button>
-                <button onClick={showAlert}>ALERT</button>
 
                 <DragonTable
                     fetchData={crudReadMany}
@@ -92,16 +79,6 @@ function Home({ pageTitle }) {
                     setTableReloadParentState={setTableReload}
                 />
             </Modal>
-
-            <Modal active={modalActive} setActive={setModalActive}>
-                <DataTable/>
-            </Modal>
-
-            <Alert
-                message="This is a custom alert!"
-                isActive={alertActive}
-                onClose={() => setAlertActive(false)}
-            />
         </>
     )
 }
